c9d5e94346ef2d98cafba44f291a183d348217eb
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-06 23:04:27 +0100

    update plugin; move voice from build script into custom config file

--- voice.groovy
+++ voice.groovy
@@ -1,5 +1,15 @@
-license {
-    name = 'Creative Commons Attribution-NoDerivs 3.0 Unported'
-    shortName = 'CC-BY-ND'
-    url = 'http://mary.dfki.de/download/by-nd-3.0.html'
+voice {
+    name = 'dfki-prudence'
+    language = 'en'
+    region = 'GB'
+    gender = 'female'
+    type = 'unit selection'
+    description = 'A female British English expressive unit selection voice: Pragmatic Prudence'
+    license {
+        name = 'Creative Commons Attribution-NoDerivs 3.0 Unported'
+        shortName = 'CC-BY-ND'
+        url = 'http://mary.dfki.de/download/by-nd-3.0.html'
+    }
+    samplingRate = 16000
+    wantsToBeDefault = 20
 }
